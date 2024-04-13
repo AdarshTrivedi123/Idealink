537dc97806bd7129b05dba4c803d5c3331a8f498
AdarshTrivedi123 <adarshtrivedi20022020@gmail.com>
2024-04-13 12:21:51 +0530

    Hosting to vercel4

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
 echo "BUILD START"
-mkdir -p python_deps
-python3.9 -m pip install -r requirements.txt -t python_deps
-mkdir -p staticfiles_build
-python3.9 manage.py collectstatic --noinput --clear --directory=staticfiles_build
-
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
